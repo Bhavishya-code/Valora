f5793f4f36ab71b6e36e0b81f53429f306862190
Bhavishya-code <bhavishyasangwan6543@gmail.com>
2025-12-09 19:22:48 +0530

    Project Setup

---
+++ apps/valora-backend/src/main/java/com/valora/backend/ValoraBackendApplication.java
@@ -0,0 +1,13 @@
+package com.valora.valora_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValoraBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValoraBackendApplication.class, args);
+	}
+
+}
---
+++ apps/valora-backend/src/test/java/com/valora/valora_backend/ValoraBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valora.valora_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValoraBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
